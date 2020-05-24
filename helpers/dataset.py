52636120917110c7745d093cd3c369b5bf267dfb
Craig Leek <cqhd.leek@gmail.com>
2020-05-25 00:56:09 +0200

    Move unknown type of enums to last place for more efficient looping

--- helpers/dataset.py
+++ helpers/dataset.py
@@ -2,7 +2,7 @@ from enum import Enum
 
 # Enum with all datasets and their names, used to select proper ctgan model and sql table
 class Dataset(Enum):
-    UNKNOWN = '?'
     DIAB = 'diabetes'
     KIDNEY = 'kidney'
     BREAST = 'breast'
+    UNKNOWN = '?'
--- helpers/mode.py
+++ helpers/mode.py
@@ -2,6 +2,6 @@ from enum import Enum
 
 # Enum to select the type of query being done, includes the name to look for and the function to execute for the query
 class Mode(Enum):
-    UNKNOWN = {'name': 0, 'func' : 0}
     AVG = {'name': 'AVG', 'func' : lambda d: d.mean()}
     SUM = {'name': 'SUM', 'func' : lambda d: d.sum()}
+    UNKNOWN = {'name': 0, 'func' : 0}
