1873cdd0343ffdb447b4ee43415ef5ec74a117d9
Craig Leek <cqhd.leek@gmail.com>
2020-05-25 09:06:38 +0200

    Move comparison from lambdas to native operator library

--- helpers/comparisontype.py
+++ helpers/comparisontype.py
@@ -1,12 +1,13 @@
 from enum import Enum
+import operator
 
 # Allowed comparison functions in query
 # IMPORTANT : Order matters, no function whose regex is a substring of another can precede the other
 class ComparisonType(Enum):
-    GTE = {'name': '>=' , 'regex' : '(?:>=)', 'func' : lambda data, value: data >= value}
-    LTE = {'name': '<=' , 'regex' : '(?:<=)', 'func' : lambda data, value: data <= value}
-    NE = {'name': '!=' , 'regex' : '(?:!=)', 'func' : lambda data, value: data != value}
-    E = {'name': '=' , 'regex' : '=', 'func' : lambda data, value : data == value}
-    GT = {'name': '>' , 'regex' : '>', 'func' : lambda data, value: data > value}
-    LT = {'name': '<' , 'regex' : '<', 'func' : lambda data, value: data < value}
+    GTE = {'name': '>=' , 'regex' : '(?:>=)', 'func' : operator.ge}
+    LTE = {'name': '<=' , 'regex' : '(?:<=)', 'func' : operator.le}
+    NE = {'name': '!=' , 'regex' : '(?:!=)', 'func' : operator.ne}
+    EQ = {'name': '=' , 'regex' : '=', 'func' : operator.eq}
+    GT = {'name': '>' , 'regex' : '>', 'func' : operator.gt}
+    LT = {'name': '<' , 'regex' : '<', 'func' : operator.lt}
     UNKNOWN = {'name': 'UNKNOWN' , 'regex' : '(?:UNKNOWN)', 'func' : lambda data, value : False}
